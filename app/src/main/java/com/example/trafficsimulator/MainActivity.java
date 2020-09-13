e6b5d4df6e64f0c554efb7e122923fbbeb9ed090
fkhan698 <fkhan698@hotmail.com>
2020-09-13 14:42:28 -0500

    2nd commmit

--- app/src/main/java/com/example/trafficsimulator/MainActivity.java
+++ app/src/main/java/com/example/trafficsimulator/MainActivity.java
@@ -2,28 +2,46 @@ package com.example.trafficsimulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
+    public class MainActivity extends AppCompatActivity {
+        int count = 0;
 
 
-    public void stopButton(View view){
-        final Button button = findViewById(R.id.button);
-        Imageview image = findViewById(R.id.greenlight);
-        button.setBackgroundColor(getResources().getColor(R.color.red));
-        button.setText("Go");
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+
+        public void stopButton(View view){
+            ImageView img = findViewById(R.id.redLightImage);
+            Button button = findViewById(R.id.button);
+            button.setText("Stop");
+
+            if(count == 0){
+                img.setImageResource(R.drawable.redlight);
+                button.setBackgroundColor(getResources().getColor(R.color.red));
+                count = 1;
+            }
+            else if(count == 1){
+                img.setImageResource(R.drawable.greenlight);
+                button.setText("Go");
+                button.setBackgroundColor(getResources().getColor(R.color.greenLight));
+                count = 2;
+
+            }else if(count == 2 ){
+                img.setImageResource(R.drawable.yellowlight);
+                button.setText("Wait");
+                count = 0;
+            }
+
+
+        }
 
 }
